a41ba7e3c9bdb58571fa431acada9409a366e8c2
scharanjit <charanjit.singh@imaginea.com>
2016-08-25 12:57:21 +0530

    Add business logic in service layer & upgraded controller

--- grails-app/controllers/org/imaginea/AccountController.groovy
+++ grails-app/controllers/org/imaginea/AccountController.groovy
@@ -2,44 +2,19 @@ package org.imaginea
 
 class AccountController {
 
+    def accountService
 
+    def index(){
+
+    }
     def transfer(){
-        def amount=params.amount.toInteger()
-
-        deposit(params.to,amount)
-        withdraw(params.from,amount)
+       accountService.transfer(params.from,params.to,params.amount.toInteger())
         redirect(action:"balances")
-
     }
 
-    private def withdraw(from,amount){
-     def account =Account.findByAccountName(from)
-        account.balance -= amount
-        account.save()
-        if(account.errors){
-            println account.errors
-        }
 
-    }
-
-    private def deposit(to,amount){
-        def account =Account.findByAccountName(to)
-        account.balance += amount
-        account.save()
-        if(account.errors){
-            println account.errors
-        }
-
-    }
 
     def balances(){
-
-        def checking_balances=Account.findByAccountName("checking").balance
-        def saving_balances=Account.findByAccountName("saving").balance
-
-        render "Checking Balance = " + checking_balances+"\n"+
-                "Saving Balance = " + saving_balances
-
-
+        render accountService.balances()
     }
 }
--- grails-app/services/org/imaginea/AccountService.groovy
+++ grails-app/services/org/imaginea/AccountService.groovy
@@ -5,7 +5,38 @@ import grails.transaction.Transactional
 @Transactional
 class AccountService {
 
-    def serviceMethod() {
+    def transfer(from,to,amount){
 
+        deposit(to,amount)
+        withdraw(from,amount)
+
+    }
+
+    private def withdraw(from,amount){
+        def account =Account.findByAccountName(from)
+        account.balance -= amount
+        account.save()
+        if(account.errors){
+            println account.errors
+        }
+
+    }
+
+    private def deposit(to,amount){
+        def account =Account.findByAccountName(to)
+        account.balance += amount
+        account.save()
+        if(account.errors){
+            println account.errors
+        }
+
+    }
+
+    def balances(){
+        def checking_balances = Account.findByAccountName("checking").balance
+        def saving_balances = Account.findByAccountName("saving").balance
+
+        return "Checking Balance = " + checking_balances+"\n"+
+          "Saving Balance = " + saving_balances
     }
 }
